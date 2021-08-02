ba7b31287d981285b1b03a281db7dfc2bd8e2721
Finnald <58459911+Finnald@users.noreply.github.com>
2021-08-02 12:19:57 +0800

    first order

---
+++ main.py
@@ -0,0 +1,24 @@
+running = 1
+cap = 0
+lat = 0
+esp = 0
+iced = 0
+while running == 1:
+    mode = input("New Order/Daily Summary: ")
+    while mode == "New Order":
+        print("Please order from the following: Cappuccino, Latte, Espresso, Iced Coffee")
+        order1 = input("")
+        if order1 == "Cappuccino":
+            cap = cap + 1
+            print("Item Added!")
+        elif order1 == "Latte":
+            lat = lat + 1
+            print("Item Added!")
+        elif order1 == "Espresso":
+            esp = esp + 1
+            print("Item Added!")
+        elif order1 == "Iced Coffee":
+            iced = iced + 1
+            print("Item Added!")
+        else:
+            print("Invalid Input")
