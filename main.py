989c444ffd0f47f26d08445efefae9bd8a8e9797
Finnald <copsnrobbers95@gmail.com>
2021-08-03 20:31:33 +0800

    added totals for daily summary

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-cap = 0
-lat = 0
-esp = 0
-iced = 0
-ordertype = " "
+capTotal = 0
+latTotal = 0
+espTotal = 0
+icedTotal = 0
+dine = 0
+take = 0
 pricedict = {
     "Cappuccino": 3.00,
     "Espresso": 2.25,
@@ -13,16 +14,22 @@ pricedict = {
 while True:
     mode = input("New Order/Daily Summary: ")
     if mode == "New Order":
-        orderdict = {
-        }
+        orderdict = {}
+        ordertype = " "
+        items = 0
+        cap = 0
+        lat = 0
+        esp = 0
+        iced = 0
         while mode == "New Order":
-            items = 0
             while ordertype == " ":
                 ordertypechoice = input("Dine in or Take out? ")
                 if ordertypechoice == "Dine in":
                     ordertype = ordertypechoice
+                    dine += 1
                 elif ordertypechoice == "Take out":
                     ordertype = ordertypechoice
+                    take += 1
                 else:
                     print("Invalid Input")
 
@@ -33,20 +40,24 @@ while True:
                 if order == "Cappuccino":
                     items += 1
                     cap += 1
+                    capTotal += 1
                     orderdict["Cappuccino"] = cap
                     print("Item Added!")
                 elif order == "Latte":
                     items += 1
                     lat += 1
+                    latTotal += 1
                     orderdict["Latte"] = lat
                     print("Item Added!")
                 elif order == "Espresso":
                     items += 1
+                    espTotal += 1
                     esp += 1
                     orderdict["Espresso"] = esp
                     print("Item Added!")
                 elif order == "Iced Coffee":
                     items += 1
+                    icedTotal += 1
                     iced += 1
                     orderdict["Iced Coffee"] = iced
                     print("Item Added!")
@@ -59,6 +70,8 @@ while True:
             for key, value in orderdict.items():
                 print(f"{value} {key}")
             confirm = input("Confirm? (Yes/No)")
+            mode = " "
+
     elif mode == "Daily Summary":
         print()
     else:
