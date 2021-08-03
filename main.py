2c75ed3a81441899920e07829117a17b59df8884
Finnald <58459911+Finnald@users.noreply.github.com>
2021-08-03 09:19:21 +0800

    added input validation for mode select

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-running = 1
 cap = 0
 lat = 0
 esp = 0
 iced = 0
-while running == 1:
+pricedict = {
+    "Cappuccino": 3.00,
+    "Espresso": 2.25,
+    "Latte": 2.50,
+    "Iced Coffee": 2.50
+}
+
+while True:
     mode = input("New Order/Daily Summary: ")
-    while mode == "New Order":
-        print("Please order from the following: Cappuccino, Latte, Espresso, Iced Coffee")
-        order1 = input("")
-        order = []
-        if order1 == "Cappuccino":
-            cap = cap + 1
-            print("Item Added!")
-        elif order1 == "Latte":
-            lat = lat + 1
-            print("Item Added!")
-        elif order1 == "Espresso":
-            esp = esp + 1
-            print("Item Added!")
-        elif order1 == "Iced Coffee":
-            iced = iced + 1
-            print("Item Added!")
-        else:
-            print("Invalid Input")
+    if mode == "New Order":
+        while mode == "New Order":
+            print("Please order from the following: Cappuccino, Latte, Espresso, Iced Coffee")
+            order1 = input("")
+            if order1 == "Cappuccino":
+                cap += 1
+                print("Item Added!")
+            elif order1 == "Latte":
+                lat += 1
+                print("Item Added!")
+            elif order1 == "Espresso":
+                esp += 1
+                print("Item Added!")
+            elif order1 == "Iced Coffee":
+                iced += 1
+                print("Item Added!")
+            else:
+                print("Invalid Input")
+    elif mode == "Daily Summary":
+        print()
+    else:
+        print("Invalid Input")
