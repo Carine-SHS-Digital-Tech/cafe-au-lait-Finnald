ea227c37d98d92cd2372d57f58a46252e7924171
Finnald <copsnrobbers95@gmail.com>
2021-08-04 19:04:15 +0800

    implemented daily summary

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ espTotal = 0
 icedTotal = 0
 dine = 0
 take = 0
+totalinc = 0
+totalorders = 0
 pricedict = {
     "Cappuccino": 3.00,
     "Espresso": 2.25,
@@ -73,20 +75,34 @@ while True:
                 print(f"{value} {key}")
             confirm = " "
             while confirm == " ":
-                confirmchoice = input("Confirm order? (Yes/No)")
+                confirmchoice = input("Confirm order? (Yes/No) ")
                 if confirmchoice == "Yes":
+                    totalorders += 1
                     confirm = confirmchoice
                     gtotal = 0
                     print("{:<15} {:<10} {:<20} {:<20} {:<10} {:<15} {:<10}".format("Item", "Quantity", "Single Item Price", "Price Excl. GST", "GST", "Extra Charges", "Total"))
                     for key, value in orderdict.items():
                         print("{:<15} {:<10} {:<20} {:<20} {:<10} {:<15} {:<10}".format(key, value, pricedict[key], value*pricedict[key], round(value*pricedict[key]*0.1, 2), round((value*0.1+value*pricedict[key])*charge,2), round((value*pricedict[key]+value*pricedict[key]*0.1+(value*0.1+value*pricedict[key])*charge), 2)))
                         gtotal += round(value*pricedict[key]+value*pricedict[key]*0.1+(value*0.1+value*pricedict[key])*charge, 2)
-                    print(f"Grand Total: {gtotal}")
+                        totalinc += gtotal
+                    print(f"Grand Total: {round(gtotal, 2)}")
+                    re = input("Print receipt? (Yes/No) ")
+                    if re == "Yes":
+                        print("Receipt printed")
+                    else:
+                        print()
                 else:
                     print("Invalid Input")
             mode = " "
 
     elif mode == "Daily Summary":
+        print("{:<15} {:<15}".format("Menu Item", "Frequency"))
+        print("{:<15} {:<15}".format("Cappuccino", capTotal))
+        print("{:<15} {:<15}".format("Espresso", espTotal))
+        print("{:<15} {:<15}".format("Latte", latTotal))
+        print("{:<15} {:<15}".format("Iced Coffee", icedTotal))
         print()
+        print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format("Dine in Freq.", "Take out Freq.", "Total Orders", "Total Cups", "Total Income", "Total GST"))
+        print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(dine, take, totalorders, capTotal+espTotal+latTotal+icedTotal, round(totalinc, 2), round(totalinc/1.1*0.1, 2)))
     else:
         print("Invalid Input")
