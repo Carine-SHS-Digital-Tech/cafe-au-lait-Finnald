570a171d2c5e67dab182ae3adb6d95ec8527c2f2
Finnald <copsnrobbers95@gmail.com>
2021-08-04 21:05:13 +0800

    Added hella comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#initialises variables
 import csv
 capTotal = 0
 latTotal = 0
@@ -14,17 +15,17 @@ pricedict = {
     "Iced Coffee": 2.50
 }
 
-while True:
+while True:     #never turns off unless break
     mode = input("New Order/Daily Summary: ")
     if mode == "New Order":
-        orderdict = {}
+        orderdict = {}      #empty dictionary for later
         ordertype = " "
         items = 0
         cap = 0
         lat = 0
         esp = 0
         iced = 0
-        while mode == "New Order":
+        while mode == "New Order":          #asks for the order type
             while ordertype == " ":
                 ordertypechoice = input("Dine in or Take out? ")
                 if ordertypechoice == "Dine in":
@@ -38,11 +39,11 @@ while True:
                 else:
                     print("Invalid Input")
 
-            print("Please order from the following; Cappuccino, Latte, Espresso, Iced Coffee: ")
+            print("Please order from the following; Cappuccino, Latte, Espresso, Iced Coffee: ")        #building the customer's order
             print("or press enter to end the order. ")
-            while items < 4:
+            while items < 4:            #max item count of 4
                 order = input("")
-                if order == "Cappuccino":
+                if order == "Cappuccino":       #adds the coffee type to an empty dictionary to make the sales receipt an appropriate size
                     items += 1
                     cap += 1
                     capTotal += 1
@@ -68,52 +69,52 @@ while True:
                     print("Item Added!")
                 elif order == "":
                     print("Order ended")
-                    break
+                    break                       #if the customer decides they want less than 4 items, leaves the order loop
                 else:
-                    print("Invalid Input")
+                    print("Invalid Input")      #if an irrelevant value is entered, lets the user try again
             print("Confirm the order:")
             for key, value in orderdict.items():
-                print(f"{value} {key}")
+                print(f"{value} {key}")                 #prints all the items in the order for confirmation
             confirm = " "
             while confirm == " ":
-                confirmchoice = input("Confirm order? (Yes/No) ")
+                confirmchoice = input("Confirm order? (Yes/No) ")   #confirms the order
                 if confirmchoice == "Yes":
                     totalorders += 1
                     confirm = confirmchoice
                     gtotal = 0
                     print("Sales receipt: ")
-                    print("{:<15} {:<10} {:<20} {:<20} {:<10} {:<15} {:<10}".format("Item", "Quantity", "Single Item Price", "Price Excl. GST", "GST", "Extra Charges", "Total"))
-                    for key, value in orderdict.items():
+                    print("{:<15} {:<10} {:<20} {:<20} {:<10} {:<15} {:<10}".format("Item", "Quantity", "Single Item Price", "Price Excl. GST", "GST", "Extra Charges", "Total"))   #headers for sales receipt
+                    for key, value in orderdict.items():        #prints the sales receipt in tabular format
                         print("{:<15} {:<10} {:<20} {:<20} {:<10} {:<15} {:<10}".format(key, value, pricedict[key], value*pricedict[key], round(value*pricedict[key]*0.1, 2), round((value*0.1+value*pricedict[key])*charge,2), round((value*pricedict[key]+value*pricedict[key]*0.1+(value*0.1+value*pricedict[key])*charge), 2)))
                         gtotal += round(value*pricedict[key]+value*pricedict[key]*0.1+(value*0.1+value*pricedict[key])*charge, 2)
                         totalinc += gtotal
                     print(f"Grand Total: ${round(gtotal, 2)}")
                     tender = ""
-                    while tender == "":
+                    while tender == "":     #asks for amount tendered to calculate change
                         while True:
-                            try:
+                            try:            #only lets the user proceed by entering a float value and doesnt break the program
                                 tendered = float(input("Amount tendered ($): "))
                                 break
                             except:
                                 print("Invalid Input")
-                        if isinstance(tendered, float):
+                        if isinstance(tendered, float):     #checks if the amount tendered is a float and then only calculates change if the amount tendered is the same or more than the grand total
                             if tendered >= gtotal:
                                 print(f"Change: ${round(tendered-gtotal, 2)}")
                                 tender = tendered
                             elif tendered < gtotal:
-                                print("Insufficient funds")
+                                print("Insufficient funds") #if the amount tendered is less than the grand total, the customer needs more money
                         else:
                             print("Invalid Input")
-                    re = input("Print receipt? (Yes/No) ")
+                    re = input("Print receipt? (Yes/No) ")  #this is the part where the employee asks the customer if they want a receipt
                     if re == "Yes":
                         print("Receipt printed")
                     else:
                         print()
                 else:
                     print("Invalid Input")
-            mode = " "
+            mode = " "      #resets the mode for selection
 
-    elif mode == "Daily Summary":
+    elif mode == "Daily Summary":       #prints daily summary contents in a tabular format
         print("{:<15} {:<15}".format("Menu Item", "Frequency"))
         print("{:<15} {:<15}".format("Cappuccino", capTotal))
         print("{:<15} {:<15}".format("Espresso", espTotal))
@@ -123,9 +124,9 @@ while True:
         print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format("Dine in Freq.", "Take out Freq.", "Total Orders", "Total Cups", "Total Income", "Total GST"))
         print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(dine, take, totalorders, capTotal+espTotal+latTotal+icedTotal, round(totalinc, 2), round(totalinc/1.1*0.1, 2)))
 
-        with open("daily_orders.csv", "w", encoding="UTF8", newline="") as f:
+        with open("daily_orders.csv", "w", encoding="UTF8", newline="") as f:   #writes the values to daily_orders.csv
             writer = csv.writer(f)
-            data = [
+            data = [                        #is the values to be written to daily_orders.csv
                 ["Menu Item", "Frequency"],
                 ["Cappuccino", capTotal],
                 ["Espresso", espTotal],
@@ -135,6 +136,6 @@ while True:
                 ["Dine in Freq.", "Take out Freq.", "Total Orders", "Total Cups", "Total Income", "Total GST"],
                 [dine, take, totalorders, capTotal+espTotal+latTotal+icedTotal, round(totalinc, 2), round(totalinc/1.1*0.1, 2)]
             ]
-            writer.writerows(data)
+            writer.writerows(data) #writes to csv
     else:
         print("Invalid Input")
